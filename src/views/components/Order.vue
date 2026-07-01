26817fe06602d6e97c6a0bda64577cd46cf75b59
monynith <monynith@gmail.com>
2026-07-01 14:22:28 +0700

    feat: add pagination to order page

--- src/views/components/Order.vue
+++ src/views/components/Order.vue
@@ -18,43 +18,48 @@
         </div>        
         <div class="table-container">
             <table class="order-table">
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>App</th>
-                    <th>Name</th>                
-                    <th>Store</th>
-                    <th>Country</th>
-                    <th class="text-right">Amount</th>                    
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody class="t-body">
-                <tr v-for="order in orders" @click="editOrder(order)">
-                    <td class="date-cell">
-                        <span class="main-text">{{ order['date'] }}</span>
-                    </td>
-                    <td class="app-logo">
-                        <img  v-if="order['app_id'] !='ID'" :src="getLogo(order['app_id'])" />
-                    </td>
-                    <td class="product-cell">{{  getName(order['app_id'])  }}</td>                
-                    <td class="total-cell">{{ order['store'] }}</td>  
-                    <td class="total-cell">{{ getCountry(order['country']) }}</td>  
-                    <td class="total-cell text-right">${{ order['pricing'] }}</td>                
-                    <td class="navi-cell text-right">
-                        <ion-icon :icon="arrowForwardOutline"></ion-icon> 
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>App</th>
+                        <th>Name</th>                
+                        <th>Store</th>
+                        <th>Country</th>
+                        <th class="text-right">Amount</th>                    
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody class="t-body">
+                    <tr v-for="order in orders" @click="editOrder(order)">
+                        <td class="date-cell">
+                            <span class="main-text">{{ order['date'] }}</span>
+                        </td>
+                        <td class="app-logo">
+                            <img  v-if="order['app_id'] !='ID'" :src="getLogo(order['app_id'])" />
+                        </td>
+                        <td class="product-cell">{{  getName(order['app_id'])  }}</td>                
+                        <td class="total-cell">{{ order['store'] }}</td>  
+                        <td class="total-cell">{{ getCountry(order['country']) }}</td>  
+                        <td class="total-cell text-right">${{ order['pricing'] }}</td>                
+                        <td class="navi-cell text-right">
+                            <ion-icon :icon="arrowForwardOutline"></ion-icon> 
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>  
+        <div id="pagination">
+            <ion-icon :icon="chevronBackOutline" @click="movePage('prev')" :style="{ opacity: ((OFFSET / LIMIT) + 1) <= 1 ? 0.3 : 0.8, cursor: ((OFFSET / LIMIT) + 1) > 1 ? 'pointer' : '' }"></ion-icon> 
+            <span>Page <b>{{ (OFFSET / LIMIT) + 1 }}</b> of {{ Math.ceil(PAGE_SIZE / LIMIT) }}</span>
+            <ion-icon :icon="chevronForwardOutline" @click="movePage('next')" :style="{ opacity: ((OFFSET / LIMIT) + 1) == Math.ceil(PAGE_SIZE / LIMIT) ? 0.3 : 0.8, cursor: ((OFFSET / LIMIT) + 1) == Math.ceil(PAGE_SIZE / LIMIT) ? '' : 'pointer' }"></ion-icon> 
+        </div>
     </div>  
 </template>
 
 <script setup lang="ts">
 import { IonIcon, IonDatetimeButton, IonDatetime, IonModal, modalController } from '@ionic/vue';
 import { createClient } from '@libsql/client';
-import { arrowForwardOutline, chevronDownOutline } from 'ionicons/icons';
+import { arrowForwardOutline, chevronBackOutline, chevronDownOutline, chevronForwardOutline } from 'ionicons/icons';
 import Skeleton from './Skeleton.vue';
 import { ref } from 'vue';
 import getCountryISO2 from 'country-iso-3-to-2';
@@ -66,6 +71,9 @@ const apps = ref([]);
 const monthModal = ref();
 const totalAmount = ref("");
 const monthYear = ref("");
+let LIMIT = 20;
+let OFFSET = 0;
+let PAGE_SIZE = 0;
 
 const changeMonth = (event: any)=> {    
     loading.value = true;
@@ -140,12 +148,48 @@ const openMonth = ()=> {
     monthModal.value.$el.present();
 }
 
+const movePage = async (type: string) => {    
+    if(type == 'next' && ((OFFSET / LIMIT) + 1) == Math.ceil(PAGE_SIZE / LIMIT)){
+        return;
+    }
+    if(type == 'prev' && ((OFFSET / LIMIT) + 1) <= 1) {
+        return;
+    }
+    if(type == 'next') {
+        OFFSET = OFFSET + LIMIT;
+    } else {
+        OFFSET = OFFSET - LIMIT;
+    }
+    loading.value = true;
+    const month = monthYear.value.split(' ')[0];
+    const year = monthYear.value.split(' ')[1];
+
+    const dbUrl = import.meta.env.VITE_DB_URL;
+    const dbToken = import.meta.env.VITE_DB_TOKEN;
+
+    const client = createClient({
+        url: dbUrl,
+        authToken: dbToken,
+    });
+    const result = await client.execute({
+        sql: `
+            SELECT * FROM orders 
+            WHERE date LIKE '%${month}%${year}' 
+            AND status = 'active' 
+            ORDER BY id DESC LIMIT ${LIMIT} OFFSET ${OFFSET}
+        `
+    });    
+    (orders.value as any) = result.rows;
+    loading.value = false;
+}
+
 const getOrders = async (date: any) => {
 
     // get current month
     // const now = new Date();
     // console.log(date);
-    const month = new Intl.DateTimeFormat('en-US', { month: 'long' }).format(date);
+    // const month = new Intl.DateTimeFormat('en-US', { month: 'long' }).format(date);
+    const month = 'June';
     const year = date.getFullYear();
 
     monthYear.value = `${month} ${year}`
@@ -164,7 +208,7 @@ const getOrders = async (date: any) => {
             SELECT * FROM orders 
             WHERE date LIKE '%${month}%${year}' 
             AND status = 'active' 
-            ORDER BY id DESC LIMIT 100
+            ORDER BY id DESC LIMIT ${LIMIT} OFFSET ${OFFSET}
         `
     }, {
         sql: `
@@ -172,10 +216,17 @@ const getOrders = async (date: any) => {
             WHERE date LIKE '%${month}%${year}' 
             AND status = 'active'             
         `
+    }, {
+        sql: `
+            SELECT count(id) as page_size FROM orders 
+            WHERE date LIKE '%${month}%${year}' 
+            AND status = 'active'  
+        `
     }]);
     (totalAmount.value as any) = result[2].rows['0']['total'];
     (apps.value as any) = result[0].rows;
     (orders.value as any) = result[1].rows;
+    (PAGE_SIZE as any) = result[3].rows[0]['page_size'];
     loading.value = false;
 }
 
