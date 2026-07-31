5c95e61c6203c0133e20072372afb323210819ed
monynith <monynith@gmail.com>
2026-07-31 22:13:18 +0700

    feat: update prompt for certification

--- src/assets/prompts/certification.ts
+++ src/assets/prompts/certification.ts
@@ -2,7 +2,7 @@ export default `
 Give me $RP{app-name}
 
 - Don't mix with any other certs
-- Each cert will be 1560. Example 3 certs: 1560*3. Make sure to reflect correctly in desc
+- Each cert will be 1560. Example 3 certs: 1560*3. Make sure to reflect correctly in desc. No need to mention of that 1560.
 - Don't mention of any N-O, Eqiz, and other personalize
 - For courseName, pls provide on in JSON format
 `
